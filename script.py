ec2aae30587e2e11ec90a8058b69c927253c40d3
feiler98 <werner.feiler@ukmuenster.de>
2025-12-09 10:51:02 +0100

    pyomics_metacell pipeline fixes

--- script.py
+++ script.py
@@ -2,15 +2,18 @@
 # ----------------------------------------------------------------------------------------------------------------------
 from pyomics import utils as ut
 from pathlib import Path
+import pandas as pd
 import scanpy as sc
 from logging_utils import log_out
 from split_hca_utils import adata_split_by_tissue
+import urllib.request
 # ----------------------------------------------------------------------------------------------------------------------
 
 # pathlib.Path raw/in/out
-path_raw = Path(__file__) / "raw_data"
-path_in = Path(__file__) / "data"
-path_out = Path(__file__) / "metacells_out"
+path_raw = Path(__file__).parent / "raw_data"
+print(path_raw)
+path_in = Path(__file__).parent / "data"
+path_out = Path(__file__).parent / "metacells_out"
 
 # create raw_data and data if not exists
 if not path_raw.exists():
@@ -23,6 +26,23 @@ if not path_in.exists():
 # logging of stdin and stderr
 log_out(path_log=path_out, log_name=f"{Path(__file__).stem}__log")
 
+# retrieve CellxGene Data as h5ad in raw_data folder
+# ----------------------------------------------------------------------------------------------------------------------
+print("""
+###############################
+# Download CellxGene datasets #
+###############################
+""")
+
+df_data_url = pd.read_csv(path_raw / "data_download_partial.txt", index_col="tag", sep=", ", engine="python")
+for tags in list(df_data_url.index):
+    url = df_data_url.loc[tags, "link"]
+    retrive_as_path = path_raw / f"{tags}.h5ad"
+    urllib.request.urlretrieve(url, retrive_as_path)
+    print(f"Dataset < {url} > has been retrieved as {retrive_as_path}")
+
+# split data and save as .h5 in data
+# ----------------------------------------------------------------------------------------------------------------------
 print("""
 ################################
 # HCA file splitting by tissue #
@@ -34,6 +54,8 @@ for p in list_paths_h5ad:
     import_adata = sc.read_h5ad(p, chunk_size=1000)
     adata_split_by_tissue(import_adata, p.stem, path_in)
 
+# metacell generation using SeaCells
+# ----------------------------------------------------------------------------------------------------------------------
 print("""
 ####################################
 # SEACells metacell generation log #
@@ -51,9 +73,10 @@ for p_h5 in list_paths_h5ad:
     adata = sc.read_h5ad(p_h5)
     sc.pp.highly_variable_genes(adata, n_top_genes=20000, subset=True, flavor="seurat_v3_paper")
     ut.scrna_dim_red_by_metacells(adata=adata,
-                                  data_tag=f"non_neuro_{p_h5.stem}",
+                                  data_tag=p_h5.stem,
                                   ad_obs_cell_type_assignment="cell_type",
-                                  path_out=path_folder_seacells_out)
+                                  path_out=path_out / f"{p_h5.stem}__seacells")
+
 
 if __name__ == "__main__":
     pass
